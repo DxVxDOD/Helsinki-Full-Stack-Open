41d053f4a91797fed566dbd8285f5863f48b9005
davidOrbang <orbangdavid@yahoo.com>
2023-04-22 16:29:46 +0100

    1.7: unicafe step2

--- part1/unicafe/src/App.tsx
+++ part1/unicafe/src/App.tsx
@@ -6,8 +6,27 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const allFeedback = () => good + bad + neutral;
+
+  const averageFeedback = () => (good + (bad * -1)) / allFeedback();
+
+  const positiveFeedback = () => (good / allFeedback()) * 100;
+
   return (
     <>
+    <h1>Give Feedback</h1>
+    <button onClick={() => setGood(good + 1)} >Good</button>
+    <button onClick={() => setNeutral(neutral + 1)} >Neutral</button>
+    <button onClick={() => setBad(bad + 1)} >Bad</button>
+    <h1>Statistics</h1>
+    <ul>
+      <li>Good: {good}</li>
+      <li>Neutral: {neutral}</li>
+      <li>Bad: {bad}</li>
+      <li>All: {allFeedback()}</li>
+      <li>Average: {averageFeedback()}</li>
+      <li>Positive: {positiveFeedback()}</li>
+    </ul>
     </>
   )
 }
